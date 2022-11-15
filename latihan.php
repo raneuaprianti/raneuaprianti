da1579bb55ff8def9d7789da09daeae818376aee
raneuaprianti <raneunagano@gmail.com>
2022-11-15 22:46:59 +0700

    first commit

---
+++ latihan.php
@@ -0,0 +1,22 @@
+<?php
+
+
+// $raneu = " raneu ";
+// echo 10;
+
+$gararetek = [["raneu","tayo"],  ["raneu","tayo"]];
+// var_dump($gararetek);
+// echo 10;
+
+foreach($gararetek as $raneu){
+    foreach($raneu as $RA){
+    echo $RA;
+    }
+}
+
+// for($i=0;$i<10;$i++){
+//     echo "raneu";
+// }
+
+
+?>
